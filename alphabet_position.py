5c6e6fbfa262b575aabdc99c6e370565b06e3822
Anton Yablonovsky <anton128355@gmail.com>
2021-09-20 20:01:12 +0300

    Add description for task

--- alphabet_position.py
+++ alphabet_position.py
@@ -1,3 +1,4 @@
+# Replace With Alphabet Position
 def alphabet_position(text):
     d = {x: y for x, y in list(zip('abcdefghijklmnopqrstuvwxyz', range(1, 28)))}
     return " ".join([" ".join([str(d[j]) for j in i if j in d ]) for i in text.lower().split()])
